dc5cbf2c41930c6bef44837220b1aa966e123cdb
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2020-06-30 23:25:12 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 'use strict';
 
 {
-  function createColum(col) {
-    const source = [];
-    for (let i = 0; i < 15; i++) {
-      source[i] = i + 1 + 15 * col;
-    }
+ 
 
-  }
-
-  const b = [];
-
-  for (let i = 0; i < 5; i++) {
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  }
-
-  console.log(b);
 }
